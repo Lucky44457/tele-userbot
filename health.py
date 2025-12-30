dc861d5da8d2d21b0f28d44ced74f51238011d7a
Lucky44457 <maribelwate@gmail.com>
2025-12-30 17:13:17 +0530

    Create health.py

---
+++ health.py
@@ -0,0 +1,11 @@
+# health.py
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+server = HTTPServer(("0.0.0.0", 5000), Handler)
+server.serve_forever()
